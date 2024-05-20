06670c54c66db09d73f87efb49a8c12604f1248a
Renan <67446339+MrCoffeeOri@users.noreply.github.com>
2024-05-20 19:28:40 -0300

    Update LinkScroll.tsx

--- src/components/LinkScroll.tsx
+++ src/components/LinkScroll.tsx
@@ -1,5 +1,5 @@
 import { Link, LinkProps } from 'react-router-dom'
 
 export default function LinkScroll(props: LinkProps) {
-    return <Link {...props} to={props.to} onClick={() => window.scrollTo({ top: 0, left: 0, behavior: 'instant' })}>{props.children}</Link>
+    return <Link {...props} to={props.to} onClick={() => window.scrollTo({ top: 0, left: 0, behavior: 'auto' })}>{props.children}</Link>
 }
