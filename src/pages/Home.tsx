61624bbb02c07d5064dce93dcabbabf366fc56f5
SrCoffee <rpenhamarquette@gmail.com>
2024-05-20 19:23:59 -0300

    home almost fixed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,12 @@ export default function Home() {
   return (
     <div id='home'>
       <nav>
-        
+        <img src="/eye.svg" alt="" />
+        <div>
+          <img src="/eye.svg" alt="" />
+          <img src="/wallet.svg" alt="" />
+          <img src="/wrench.svg" alt="" />
+        </div>
       </nav>
       <div id='dashboard'>
         <header></header>
