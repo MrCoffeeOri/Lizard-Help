16d7497ff6db4b368f65c67c28a5aed13a903f11
SrCoffee <rpenhamarquette@gmail.com>
2024-10-06 22:24:43 -0300

    sasdsad

--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -12,73 +12,96 @@ export default function Start() {
     e.preventDefault();
     if (document.querySelector("form > input[error]")) return
     scrollTo({ top: 0, left: 0 })
-    await fetch("http://localhost:5000/company/create", {
+    fetch("http://localhost:5000/user/create", {
       method: "POST",
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ 
-        owner: { 
-          name: (e.currentTarget[0] as HTMLInputElement).value, 
-          email: (e.currentTarget[1] as HTMLInputElement).value 
-        }, 
-        name: (e.currentTarget[2] as HTMLInputElement).value, 
-        email: (e.currentTarget[3] as HTMLInputElement).value, 
-        password: (e.currentTarget[4] as HTMLInputElement).value, 
-        tel: (e.currentTarget[6] as HTMLInputElement).value, 
-        cid: (e.currentTarget[7] as HTMLInputElement).value 
+        name: (e.target[0] as HTMLInputElement).value, 
+        email: (e.target[1] as HTMLInputElement).value,
+        password: (e.target[2] as HTMLInputElement).value, 
+        type: 'owner'
       })
     })
+      .then(res => res.json())
+      .then(user => {
+        fetch("http://localhost:5000/company/create", {
+          method: "POST",
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ 
+            name: (e.target[4] as HTMLInputElement).value,
+            email: (e.target[5] as HTMLInputElement).value, 
+            phone: (e.target[6] as HTMLInputElement).value, 
+            cid: (e.target[7] as HTMLInputElement).value,
+            owner: user._id
+          })
+      })
+      })
   };
 
   return (
     <div id='start'>
       <GoBack to='/' />
       <img id='logo' src="./logoName.webp" alt="" />
-      <div>
+    <div>
         <form className='form' onSubmit={handleSubmit}>
-          <img src="./logo.webp" alt="" />
-          <h2>Cadastrar</h2>
-          <input
-            required
-            type="text"
-            placeholder="Nome da Empresa"
-          />
-          <input
-            required
-            type="email"
-            placeholder="Email da Empresa"
-            onChange={e => handleCorrection(e, e => e.target.value.match(RegexCorrections.email) != null)}
-          />
-          <p>Use o formato username@domain.com</p>
-          <input
-            required
-            type="password"
-            placeholder="Senha"
-            ref={passwordRef}
-          />
-          <input
-            required
-            type="password"
-            placeholder="Repetir Senha"
-            onChange={e => handleCorrection(e, e => e.target.value == passwordRef.current.value)}
-          />
-          <p>Senha não corresponde</p>
-          <input 
-            required
-            type="tel" 
-            placeholder="Número de telefone" 
-            onChange={e => handleCorrection(e, e => e.target.value.match(RegexCorrections.telNumber) != null)}
-          />
-          <p>Use o formato (XX) XXXXX-XXXX</p>
-          <input
-            required
-            type="text"
-            placeholder="CNPJ"
-            onChange={e => handleCorrection(e, e => e.target.value.match(RegexCorrections.companyID) != null)}
-          />
-          <p>Use o formato XX.XXX.XXX/0001-XX</p>
-          <button style={{ marginBottom: 15 }} type="submit">Criar</button>
-          <div>
-            <Link rel="stylesheet" to="/client#login">Já possui uma conta?</Link>
-          </div>
+        <img src="./logo.webp" alt="" />
+        <h2>Dados do Usuário</h2>
+        <input
+          required
+          type="text"
+          placeholder="Nome Completo"
+        />
+        <input
+          required
+          type="email"
+          placeholder="Email Pessoal"
+          onChange={e => handleCorrection(e, e => e.target.value.match(RegexCorrections.email) != null)}
+        />
+        <p>Use o formato username@domain.com</p>
+        <input
+          required
+          type="password"
+          placeholder="Senha"
+          ref={passwordRef}
+        />
+        <input
+          required
+          type="password"
+          placeholder="Repetir Senha"
+          onChange={e => handleCorrection(e, e => e.target.value == passwordRef.current.value)}
+        />
+        <p>Senha não corresponde</p>
+        <h2 style={{ marginTop: 30 }}>Dados da Empresa</h2>
+        <input
+          required
+          type="text"
+          placeholder="Nome da Empresa"
+        />
+        <input
+          required
+          type="email"
+          placeholder="Email da Empresa"
+          onChange={e => handleCorrection(e, e => e.target.value.match(RegexCorrections.email) != null)}
+        />
+        <p>Use o formato username@domain.com</p>
+        <input 
+          required
+          type="tel" 
+          placeholder="Número de telefone" 
+          onChange={e => handleCorrection(e, e => e.target.value.match(RegexCorrections.telNumber) != null)}
+        />
+        <p>Use o formato (XX) XXXXX-XXXX</p>
+        <input
+          required
+          type="text"
+          placeholder="CNPJ"
+          onChange={e => handleCorrection(e, e => e.target.value.match(RegexCorrections.companyID) != null)}
+        />
+        <p>Use o formato XX.XXX.XXX/0001-XX</p>
+        <button style={{ marginBottom: 15 }} type="submit">Criar</button>
+        <div>
+          <Link rel="stylesheet" to="/client#login">Já possui uma conta?</Link>
+        </div>
         </form>
         <h1>Centralize seus canais de atendimento com <span className="marked">gerenciamento</span> e <span className="marked">automação</span> completos de tickets</h1>
       </div>
