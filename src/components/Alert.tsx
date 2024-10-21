60486cfbf9903ab720ac9baa42a42838e4130e90
Renan <rpenhamarquette@gmail.com>
2024-10-21 11:32:11 -0300

    alert melhorado

---
+++ src/components/Alert.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useRef } from 'react';
+import styles from '../css/modules/alert.module.css'
+
+export default function Alert({ message, time = 6500 }: { message: string | null, time?: number }) {
+  const alertRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      alertRef.current.remove(); 
+    }, time);
+    return () => clearTimeout(timer); 
+  }, []);
+
+  const handleCloseAlert = () => {
+    alertRef.current.remove();
+  };
+
+  return (
+    message ?
+    <div ref={alertRef} className={styles.alert}>
+      <div onClick={handleCloseAlert}>
+        <p>{message}</p>
+        <div></div>
+      </div>
+    </div>
+    :
+    <></>
+  );
+}
--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -1,120 +1,83 @@
-import React, { useRef } from 'react'
-import { useNavigate, Link } from 'react-router-dom'
+import React, { useEffect, useRef, useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
 import { RegexCorrections, handleCorrection } from '../helpers/formCorrection';
 import GoBack from '../components/GoBack';
 import CopyRight from '../components/CopyRight';
+import Alert from '../components/Alert';
 
 export default function Start() {
-  const passwordRef = useRef<HTMLInputElement>(null)
-  const navigate = useNavigate()
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+  const navigate = useNavigate();
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if (document.querySelector("form > input[error]")) return
-    scrollTo({ top: 0, left: 0 })
+    if (document.querySelector("form > input[error]")) return;
+    scrollTo({ top: 0, left: 0 });
+
     try {
-        const userResponse = await (await fetch("http://localhost:5000/user/create", {
-          method: "POST",
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({ 
-              name: (e.target[0] as HTMLInputElement).value, 
-              email: (e.target[1] as HTMLInputElement).value,
-              password: (e.target[2] as HTMLInputElement).value, 
-              type: 'owner'
-            })
-        })).json()
-        if (userResponse.error) return alert(userResponse.error)
-        await fetch("http://localhost:5000/company/create", {
-          method: "POST",
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({ 
-            name: (e.target[4] as HTMLInputElement).value,
-            email: (e.target[5] as HTMLInputElement).value, 
-            phone: (e.target[6] as HTMLInputElement).value, 
-            cid: (e.target[7] as HTMLInputElement).value,
-            owner: userResponse.user._id
-          })
-      })
+      const userResponse = await (await fetch("http://localhost:5000/user/create", {
+        method: "POST",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          name: (e.target[0] as HTMLInputElement).value,
+          email: (e.target[1] as HTMLInputElement).value,
+          password: (e.target[2] as HTMLInputElement).value,
+          type: 'owner'
+        })
+      })).json();
+      if (userResponse.error) {
+        setErrorMessage(userResponse.error);
+        return;
+      }
+      await fetch("http://localhost:5000/company/create", {
+        method: "POST",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          name: (e.target[4] as HTMLInputElement).value,
+          email: (e.target[5] as HTMLInputElement).value,
+          phone: (e.target[6] as HTMLInputElement).value,
+          cid: (e.target[7] as HTMLInputElement).value,
+          owner: userResponse.user._id
+        })
+      });
     } catch (error) {
-      alert(error)
+      setErrorMessage(error); 
     }
   };
 
-  const handleCloseAlert = (e: React.MouseEvent<HTMLSpanElement>) => {
-    e.currentTarget.parentElement.parentElement.remove()
-  }
-
   return (
     <div id='start'>
-      <div className='alert'>
-        <div>
-          <span onClick={handleCloseAlert}>x</span>
-          <p>ajskdbashk kgshkadgahk gasdhgashk</p>
-          <div></div>
-        </div>
-      </div>
+      <Alert message={errorMessage} />
       <GoBack to='/' />
       <img id='logo' src="./logoName.webp" alt="" />
       <div>
         <form className='form' onSubmit={handleSubmit}>
-        <img src="./logo.webp" alt="" />
-        <h2>Dados do Usuário</h2>
-        <input
-          required
-          type="text"
-          placeholder="Nome Completo"
-        />
-        <input
-          required
-          type="email"
-          placeholder="Email Pessoal"
-          onChange={e => handleCorrection(e, e => e.target.value.match(RegexCorrections.email) != null)}
-        />
-        <p>Use o formato username@domain.com</p>
-        <input
-          required
-          type="password"
-          placeholder="Senha"
-          ref={passwordRef}
-        />
-        <input
-          required
-          type="password"
-          placeholder="Repetir Senha"
-          onChange={e => handleCorrection(e, e => e.target.value == passwordRef.current.value)}
-        />
-        <p>Senha não corresponde</p>
-        <h2 style={{ marginTop: 30 }}>Dados da Empresa</h2>
-        <input
-          required
-          type="text"
-          placeholder="Nome da Empresa"
-        />
-        <input
-          required
-          type="email"
-          placeholder="Email da Empresa"
-          onChange={e => handleCorrection(e, e => e.target.value.match(RegexCorrections.email) != null)}
-        />
-        <p>Use o formato username@domain.com</p>
-        <input 
-          required
-          type="tel" 
-          placeholder="Número de telefone" 
-          onChange={e => handleCorrection(e, e => e.target.value.match(RegexCorrections.telNumber) != null)}
-        />
-        <p>Use o formato (XX) XXXXX-XXXX</p>
-        <input
-          required
-          type="text"
-          placeholder="CNPJ"
-          onChange={e => handleCorrection(e, e => e.target.value.match(RegexCorrections.companyID) != null)}
-        />
-        <p>Use o formato XX.XXX.XXX/0001-XX</p>
-        <button style={{ marginBottom: 15 }} type="submit">Criar</button>
-        <div>
-          <Link rel="stylesheet" to="/client#login">Já possui uma conta?</Link>
-        </div>
+          <img src="./logo.webp" alt="" />
+          <h2>Dados do Usuário</h2>
+          <input required type="text" placeholder="Nome Completo" />
+          <input required type="email" placeholder="Email Pessoal"
+            onChange={e => handleCorrection(e, e => e.target.value.match(RegexCorrections.email) != null)} />
+          <p>Use o formato username@domain.com</p>
+          <input required type="password" placeholder="Senha" ref={passwordRef} />
+          <input required type="password" placeholder="Repetir Senha"
+            onChange={e => handleCorrection(e, e => e.target.value == passwordRef.current.value)} />
+          <p>Senha não corresponde</p>
+          <h2 style={{ marginTop: 30 }}>Dados da Empresa</h2>
+          <input required type="text" placeholder="Nome da Empresa" />
+          <input required type="email" placeholder="Email da Empresa"
+            onChange={e => handleCorrection(e, e => e.target.value.match(RegexCorrections.email) != null)} />
+          <p>Use o formato username@domain.com</p>
+          <input required type="tel" placeholder="Número de telefone"
+            onChange={e => handleCorrection(e, e => e.target.value.match(RegexCorrections.telNumber) != null)} />
+          <p>Use o formato (XX) XXXXX-XXXX</p>
+          <input required type="text" placeholder="CNPJ"
+            onChange={e => handleCorrection(e, e => e.target.value.match(RegexCorrections.companyID) != null)} />
+          <p>Use o formato XX.XXX.XXX/0001-XX</p>
+          <button style={{ marginBottom: 15 }} type="submit">Criar</button>
+          <div>
+            <Link rel="stylesheet" to="/client#login">Já possui uma conta?</Link>
+          </div>
         </form>
         <div>
           <img src="start.png" alt="" />
