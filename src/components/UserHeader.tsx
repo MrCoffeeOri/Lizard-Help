6e31d83fb97b091304e43de356946a49cd209948
Tiago <tpaz969@gmail.com>
2024-10-28 10:43:47 -0300

    aaaaaaaaa

--- src/components/UserHeader.tsx
+++ src/components/UserHeader.tsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import styles from '../css/modules/userHeader.module.css'
+import { userContext } from './UserContext'
 
 export default function UserHeader({ children }: React.PropsWithChildren) {
+  const { user } = useContext(userContext)
   return (
     <header className={styles.userHeader}>
         <img src="/logoName.webp" alt="" />
         <div>
           {children}
           <div>
-            <span>Josias Tutano</span>
+            <span>{user.name}</span>
             <img src="https://yt3.ggpht.com/lxl6Fy8c3z1t45KBvGhdShOic82nbvM7nSR4H-g3GernzddR1ztA6W7sAVx_QQ-XxSEOVp_V=s48-c-k-c0x00ffffff-no-rj" />
           </div>
         </div>
--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useRef, useState } from 'react';
-import { useNavigate, Link, redirect } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import { RegexCorrections, handleCorrection } from '../helpers/formCorrection';
 import GoBack from '../components/GoBack';
 import CopyRight from '../components/CopyRight';
@@ -45,7 +45,7 @@ export default function Start() {
         return setErrorMessage(companyResponse.error)
       userResponse.user.company = companyResponse.company
       setUser(userResponse.user)
-      redirect("/home")
+      navigate("/home")
     } catch (error) {
       setErrorMessage(error); 
     }
