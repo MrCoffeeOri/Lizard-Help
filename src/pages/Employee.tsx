1c72f8e17c3c22dde7d98aab99c1bcb2d9097388
SrCoffee <rpenhamarquette@gmail.com>
2024-11-01 18:13:31 -0300

    employee added

---
+++ src/pages/Employee.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import UserHeader from '../components/UserHeader'
+
+export default function Employee() {
+  return (
+    <div id='employee'>
+      <UserHeader></UserHeader>
+    </div>
+  )
+}
